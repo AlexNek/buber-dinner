e90046cdf8ee964fb5f8c5fdc1893e217d9a21f4
AlexNek <alexnek@russinger.com>
2023-10-07 21:01:44 +0200

    add more ef support

---
+++ BuberDinner.Infrastructure/BuberDinnerContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using BuberDinner.Infrastructure.Persistence;
+using BuberDinner.Infrastructure.Persistence.Interceptors;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BuberDinner.Infrastructure
+{
+    public class BuberDinnerContextFactory : IDesignTimeDbContextFactory<BuberDinnerDbContext>
+    {
+        public BuberDinnerDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BuberDinnerDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BuberDinner;TrustServerCertificate=True;Integrated Security=true");
+            PublishDomainEventsInterceptor eventsInterceptor = new PublishDomainEventsInterceptor(null);
+            return new BuberDinnerDbContext(optionsBuilder.Options, eventsInterceptor);
+        }
+    }
+}
