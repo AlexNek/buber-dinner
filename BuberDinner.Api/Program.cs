cc7614932b830dcd9945ec5918300b5d1fc5ff91
AlexNek <alexnek@russinger.com>
2023-10-08 14:46:44 +0200

    reformat Program.cs

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -5,37 +5,37 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services
+    var services = builder.Services;
+
+    services
         .AddPresentation()
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-                builder.Services.AddSwaggerGen(option =>
-                    {
-                        option.SwaggerDoc("v1", new OpenApiInfo { Title = "BiberDinner API", Version = "v1" });
-                        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                                                                   {
-                                                                       In = ParameterLocation.Header,
-                                                                       Description = "Please enter a valid token",
-                                                                       Name = "Authorization",
-                                                                       Type = SecuritySchemeType.Http,
-                                                                       BearerFormat = "JWT",
-                                                                       Scheme = "Bearer"
-                                                                   });
-                        option.AddSecurityRequirement(new OpenApiSecurityRequirement
-                                                          {
-                                                              {
-                                                                  new OpenApiSecurityScheme
-                                                                      {
-                                                                          Reference = new OpenApiReference
-                                                                                          {
-                                                                                              Type=ReferenceType.SecurityScheme,
-                                                                                              Id="Bearer"
-                                                                                          }
-                                                                      },
-                                                                  new string[]{}
-                                                              }
-                                                          });
-                    });
+
+    services.AddSwaggerGen(
+        option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "BiberDinner API", Version = "v1" });
+                option.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                        {
+                            In = ParameterLocation.Header,
+                            Description = "Please enter a valid token",
+                            Name = "Authorization",
+                            Type = SecuritySchemeType.Http,
+                            BearerFormat = "JWT",
+                            Scheme = "Bearer"
+                        });
+                option.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                        {
+                            {
+                                new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" } },
+                                new string[] { }
+                            }
+                        });
+            });
 }
 
 var app = builder.Build();
