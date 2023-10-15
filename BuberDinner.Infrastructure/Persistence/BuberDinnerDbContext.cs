e378cf244389c8b8da53a387782d3026d69fd225
AlexNek <alexnek@russinger.com>
2023-10-15 20:10:48 +0200

    notes missing tables

--- BuberDinner.Infrastructure/Persistence/BuberDinnerDbContext.cs
+++ BuberDinner.Infrastructure/Persistence/BuberDinnerDbContext.cs
@@ -27,9 +27,27 @@ public sealed class BuberDinnerDbContext : DbContext
 
     public DbSet<Bill> Bills { get; set; } = null!;
     public DbSet<Dinner> Dinners { get; set; } = null!;
+    //todo?
+    // table reservation ConfigureReservationsTable?
+    //public DbSet<DinnerReservations> DinnerReservations { get; set; } = null!;
+    //public DbSet<GuestBill> GuestBillIds { get; set; } = null!;
+    //public DbSet<GuestMenuReview> GuestMenuReviewIds { get; set; } = null!;
+    //public DbSet<GuestPastDinner> GuestPastDinnerIds { get; set; } = null!;
+    //public DbSet<GuestPendingDinner> GuestPendingDinnerIds { get; set; } = null!;
+    //public DbSet<GuestRating> GuestRatings { get; set; } = null!;
     public DbSet<Guest> Guests { get; set; } = null!;
+    //todo?
+    //public DbSet<GuestUpcommingDinner> GuestUpcommingDinnerIds { get; set; } = null!;
+    //public DbSet<HostDinner> HostDinnerIds { get; set; } = null!;
+    //public DbSet<HostMenu> HostMenuIds { get; set; } = null!;
+
     public DbSet<Host> Hosts { get; set; } = null!;
     public DbSet<Menu> Menus { get; set; } = null!;
+    //public DbSet<MenuDinner> MenuDinnerIds { get; set; } = null!;
+
+    // see ConfigureMenuSectionsTable
+    //public DbSet<MenuItems> MenuItems { get; set; } = null!;
+    //BuildModel for MenuReviewIds
     public DbSet<MenuReview> MenuReviews { get; set; } = null!;
     public DbSet<User> Users { get; set; } = null!;
 
